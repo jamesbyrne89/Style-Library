1a9cba9680c07d5912d4914b8b88afc68389d0b0
jamesbyrne89 <mail@jamestbyrne.com>
2017-12-18 23:03:16 +0000

    Added Header component

---
+++ style-library/src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './style.css';
+
+const Header = () => {
+    return ( <
+        <
+        header className = "App-header" > < / header >
+    );
+}
+
+export default Header;
