5d2ba6e72b052b625220bea8b0b61f7c56d884d1
jamesbyrne89 <mail@jamestbyrne.com>
2018-03-02 22:46:26 +0000

    Convert to create-react-app

---
+++ app/src/components/Colour.js
@@ -0,0 +1,13 @@
+const Colour = (props) => {
+    const { hex, name, rgb } = props.col
+    return (
+            <div>
+                <div className="colour__sample" style={{backgroundColor: hex}}></div>
+                <span className="colour__hex">{hex}</span>
+                <span className="colour__rgb">{rgb}</span>
+            </div>
+
+    )
+}
+
+export default Colour;
---
+++ app/src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+const NavBar = () => {
+    return (
+        <header className="primary-nav">
+            <nav className="primary-nav__container">
+                <ul>
+                    {/* <li><Link href="/Colours"><a>Colours</a></Link></li>
+                    <li><Link href="/Typography"><a>Typography</a></Link></li>
+                    <li><Link href="/Components"><a>Components</a></Link></li>
+                    <li><Link href="/Buttons"><a>Buttons</a></Link></li> */}
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar;
