c15232bf2068f91413b2349df14a29dfbe40733e
jamesbyrne89 <mail@jamestbyrne.com>
2018-03-03 22:26:32 +0000

    Add animation page

---
+++ app/src/routes/Animation.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+
+
+const Animation = () => {
+    return (
+        <div>
+            <NavBar />
+            <main className="app-content">
+                <section className="content-container">
+                <h1 className="page-title">Animation</h1>
+                <article>Purpose</article>
+                <article>Focus alpha lean startup hypotheses bootstrapping. Product management vesting period seed money entrepreneur leverage learning curve graphical user interface technology network effects low hanging fruit niche market disruptive. First mover advantage supply chain agile development interaction design crowdfunding A/B testing. Gamification market ownership business model canvas.</article>
+                <article>Neutrals</article>
+                <article>
+                <ul className="colour-palette">
+                    </ul>
+
+                </article>
+                <article>Primary Animation</article>
+                <article>
+                    <ul className="colour-palette">
+                        <li>
+                            <div className="colour__sample"></div>
+                            <span className="colour__hex">#edf207</span>
+                            <span className="colour__rgb">rgb(237,242,7)</span>
+                        </li>
+                        <li>
+                            <div className="colour__sample"></div>
+                            <span className="colour__hex">#f8ede0</span>
+                            <span className="colour__rgb">rgb(248,237,224)</span>
+                        </li>
+                        <li>
+                            <div className="colour__sample"></div>
+                            <span className="colour__hex">#edf207</span>
+                            <span className="colour__rgb">rgb(237,242,7)</span>
+                        </li>
+                    </ul>
+                </article>
+                <article></article>
+                <article></article>
+                <article></article>
+                <article></article>
+                <article></article>
+                </section>
+            </main>
+        </div>
+    )
+}
+
+export default Animation;
