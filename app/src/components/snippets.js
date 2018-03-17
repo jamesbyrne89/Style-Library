141cea81b6725768bc438295fb11bad895dcad7d
jamesbyrne89 <mail@jamestbyrne.com>
2018-03-17 20:41:36 +0000

    Move customVideoPlayer into separate component

--- app/src/components/snippets.js
+++ app/src/components/snippets.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const customVideoPlayer = () => {
 
     function initCustomVideoPlayer() {
@@ -131,7 +133,26 @@ const customVideoPlayer = () => {
     }
 
     initCustomVideoPlayer();
+    return (
+                                    <div class="wh-player">
+                                            <script defer src="https://use.fontawesome.com/releases/v5.0.4/js/all.js"></script>
+                                        <video class="wh-player__video viewer" src="https://player.vimeo.com/external/194837908.sd.mp4?s=c350076905b78c67f74d7ee39fdb4fef01d12420&profile_id=164"></video>
+                                        <button class="overlay-btn">{'\u25B6'}</button>
+                                        <div class="wh-player__controls">
 
+                                            <button class="wh-player__button toggle" title="Toggle Play">{'\u25B6'}</button>
+                                            <div class="progress">
+                                                <div class="progress__filled"></div>
+                                            </div>
+                                            <button class="wh-player__button restart">{'&#x21ba'}</button>
+                                            <div class="wh-player__volume-controls">
+                                                <button class="wh-player__button volume" title="Toggle Volume"><i class="fa fa-volume-up"></i></button>
+                                                <input type="range" name="volume" class="wh-player__slider wh-player__volume" min="0" max="1" step="0.05" value="1" />
+                                            </div>
+                                            <button class="wh-player__button fullscreen"><i class="fa fa-expand"></i></button>
+                                        </div>
+                                    </div>
+    )
 }
 
 export default customVideoPlayer;
